df7255409024640601c6925805a98d29002377d1
jokagetto9 <jokagetto9@gmail.com>
2016-04-28 22:36:31 -0600

    MenuLoader Xml Reading Tests

--- MenuLoader.cpp
+++ MenuLoader.cpp
@@ -1,2 +1,80 @@
 //********************************* INITIALIZATION *********************************
 #include "MenuLoader.h"
+
+void MenuLoader::registerRootMenu(Menu * m){
+	if (m)
+		rMenus.push_back(m);
+}
+
+
+
+void MenuLoader::loadList(){
+    rapidxml::file<> xmlFile("menulist.xml"); // Default template is char
+    rapidxml::xml_document<> doc;
+	try {
+		doc.parse<0>(xmlFile.data());
+		rapidxml::xml_node<> *list = doc.first_node();
+		rapidxml::xml_node<> *n;
+		rapidxml::xml_attribute<> *a;
+		string s = getText(list->name());
+		if (s == "MenuList"){ 
+			for (n = list->first_node(); n; n = n->next_sibling()){
+				s = getText(n->name());		
+				a = n->first_attribute();
+				if (s == "Menu" && a) {
+					s = getText(a->name());
+					if (s == "filename"){
+						s = getText(a->value());
+						menuFiles.push_back(s);
+					}
+				}
+			}
+		}
+		loadMenu(0);
+	}catch(...){
+		cout << "Menu did not load properly.";
+	}
+}//*/
+
+
+
+void MenuLoader::loadMenu(int i){
+	if (i < menuFiles.size()){
+		rapidxml::file<> xmlFile(menuFiles[i].c_str()); // Default template is char
+		rapidxml::xml_document<> doc;
+		try {
+			doc.parse<0>(xmlFile.data());
+			rapidxml::xml_node<> *list = doc.first_node();
+			rapidxml::xml_node<> *n;
+			rapidxml::xml_attribute<> *a;
+			string s = getText(list->name());
+			if (s == "Menu"){ 
+				for (a = list->first_attribute(); a; a = a->next_attribute()){
+					s = getText(a->name());	
+					cout << getText(a->name()) << endl;
+				}
+			}
+		}catch(...){
+			cout << "Menu did not load properly.";
+		}
+	}
+}
+
+void MenuLoader::printList(){
+	for (int i = 0; i < menuFiles.size(); i++){
+		cout << menuFiles[i] << endl;
+	}
+}
+
+void MenuLoader::printMenu(int i){
+	if (i < menuFiles.size()){
+
+
+	}
+}
+
+string MenuLoader::getText(char * c){	
+	stringstream ss; string s;
+	ss << c;	 ss >> s;
+	return s;
+}
--- MenuLoader.h
+++ MenuLoader.h
@@ -1,13 +1,28 @@
 #ifndef MENULOADER_H
 #define MENULOADER_H
-#include "../lib.h"
+#include "Menus/Menu.h"
 
 //********************************* CONSTANTS *********************************
 
 //********************************* INITIALIZATION *********************************
 
 class MenuLoader  {
+
 public: 
+	void MenuLoader::registerRootMenu(Menu * m);
+
+
+	void MenuLoader::loadList();
+	void MenuLoader::loadMenu(int i);
+
+	void MenuLoader::printList();
+	void MenuLoader::printMenu(int i);
+
+
+	string MenuLoader::getText(char * c);
 private:
+	vector <string> menuFiles;		
+	vector <Menu *> rMenus;
+
 };
 #endif
