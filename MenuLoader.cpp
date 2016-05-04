6a38a149a3151a5b858134f5d0fb4a8ac7863df6
jokagetto9 <jokagetto9@gmail.com>
2016-05-03 20:38:24 -0600

    Implemented XMLParser

--- MenuLoader.cpp
+++ MenuLoader.cpp
@@ -13,33 +13,9 @@ void MenuLoader::registerRoot(StackCommand * m){
 
 
 
-void MenuLoader::loadList(){
-    rapidxml::file<> xmlFile("menulist.xml"); // Default template is char
-    rapidxml::xml_document<> doc;
-	try {
-		doc.parse<0>(xmlFile.data());
-		rapidxml::xml_node<> *list = doc.first_node();
-		rapidxml::xml_node<> *n;
-		rapidxml::xml_attribute<> *a;
-		string s = getText(list->name());
-		if (s == "MenuList"){ 
-			for (n = list->first_node(); n; n = n->next_sibling()){
-				s = getText(n->name());		
-				a = n->first_attribute();
-				if (s == "MenuFile" && a) {
-					s = getText(a->name());
-					if (s == "filename"){
-						s = getText(a->value());
-						menuFiles.push_back(s);
-					}
-				}
-				if (s == "CursorFile" && a) {
-					s = getText(a->name());
-					if (s == "filename")
-						cursorFile = getText(a->value());					
-				}
-			}
-		}
+void MenuLoader::load(){
+	try {		
+		loadList("menulist.xml", "MenuList", "MenuFile", menuFiles);
 	}catch(...){
 		cout << "Menus did not load properly." << endl;
 	}
@@ -48,6 +24,21 @@ void MenuLoader::loadList(){
 	}
 	loadCursors();
 }//*/
+
+void MenuLoader::loadAuxillary(rapidxml::xml_node<> * node){
+	rapidxml::xml_attribute<> * a;
+	for (node; node; node = node->next_sibling()){
+		string s = getText(node->name());
+		a = node->first_attribute();
+		if (s == "CursorFile" && a) {
+			s = getText(a->name());
+			if (s == "filename")
+				cursorFile = getText(a->value());					
+		}
+	}
+}
+
+
 
 void MenuLoader::loadMenuTree(int i){
 	if (i < menuFiles.size() && i < rMenus.size()){
@@ -168,18 +159,4 @@ void MenuLoader::printMenu(int i){
 	}
 }
 
-string MenuLoader::getText(char * c){	
-	stringstream ss; string s;
-	ss << c;	 ss >> s;
-	return s;
-}
 
-
-int MenuLoader::getInt(char * c){	
-	stringstream ss; int i;
-	ss << c;	 ss >> i;
-	if (i < -10000)
-		return 0;
-	else 
-		return i;
-}
--- MenuLoader.h
+++ MenuLoader.h
@@ -2,29 +2,28 @@
 #define MENULOADER_H
 #include "Menus/Menu.h"
 #include "../Utility/MCommand.h"
+#include "../Utility/XMLParser.h"
 
 //********************************* CONSTANTS *********************************
 
 //********************************* INITIALIZATION *********************************
 
-class MenuLoader  {
+class MenuLoader  : public XMLParser{
 public: 
 	MenuLoader::MenuLoader();
 
 	void MenuLoader::registerRoot(StackCommand * m);
 
 
-	void MenuLoader::loadList();
+	void MenuLoader::load();
 	void MenuLoader::loadMenuTree(int i);
 	void MenuLoader::loadMenu(int i, rapidxml::xml_node<> * node);
+	void MenuLoader::loadAuxillary(rapidxml::xml_node<> * node);
 	void MenuLoader::loadCursors();
 
 	void MenuLoader::printList();
 	void MenuLoader::printMenu(int i);
 
-
-	string MenuLoader::getText(char * c);
-	int MenuLoader::getInt(char * c);
 private:
 	vector <string> menuFiles;		
 	vector <StackCommand *> rMenus;
